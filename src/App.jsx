c3a9d289cf36a360dd2ee82c751c803b1c3ec3b8
gersonch <gersonch@users.noreply.github.com>
2024-09-30 16:48:30 -0300

    seccion register

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Footer } from "./components/Footer";
 
 import { Home } from "./components/Home";
 import Navbar from "./components/Navbar";
+import { Register } from "./components/Register";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Register />
+      {/*<Home />*/}
       <Footer />
     </>
   );
---
+++ src/components/Register.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+export function Register() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleConfirmPassword(e) {
+    setConfirmPassword(e.target.value);
+  }
+
+  function checkInfo(e) {
+    e.preventDefault();
+
+    if (password === confirmPassword) {
+      alert("Has creado tu cuenta exitosamente");
+    } else {
+      alert("Verifica tus datos. Las contraseñas deben ser iguales.");
+    }
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-start my-5">
+      <form className="w-25" onSubmit={checkInfo}>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            value={password}
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Password2" className="form-label">
+            Repeat Password
+          </label>
+          <input
+            value={confirmPassword}
+            type="password"
+            className="form-control"
+            id="Password2"
+            onChange={handleConfirmPassword}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
