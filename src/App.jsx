bbcaf6c3063cb48ff9bc9eb5335878e577e9d5a1
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-10-13 00:01:33 -0300

    consumir api

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import { Footer } from "./components/Footer";
 import { Banner } from "./components/Banner";
 import { Home } from "./components/Home";
 import Navbar from "./components/Navbar";
-import { Register } from "./components/Register";
-import { Login } from "./components/Login";
-import { Cart } from "./components/Cart";
+//import { Register } from "./components/Register";
+//import { Login } from "./components/Login";
+//import { Cart } from "./components/Cart";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
-      {/*<Home /> */}
+      <Home />
       {/*<Register />*/}
       {/*<Login />*/}
-      <Cart />
+      {/*<Cart />*/}
       <Footer />
     </>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,30 @@
 /* eslint-disable react/jsx-key */
 import { CardPizza } from "./CardPizza";
+import { useEffect, useState } from "react";
 import "../App.css";
-import { pizzas } from "./pizzas";
+//import { pizzas } from "./pizzas";
 import { formatPrice } from "./format.js"
 
 export function Home() {
+
+  const [data, setData] = useState([])
+  const url = 'http://localhost:5000/api/pizzas'
+
+  useEffect(() =>{
+    fetch(url)
+      .then(data => data.json())
+      .then(data => {
+        setData(data)
+      })
+  },[])
+
   return (
     <>
-
 
       <main className="">
         <section className="pizzas w-75">
           <div className="m-5 row">
-            {pizzas.map(pizza => (
+            {data.map(pizza => (
               <div className="col-12 col-md-4 mb-4 d-flex">
               <CardPizza
                 name={pizza.name}
